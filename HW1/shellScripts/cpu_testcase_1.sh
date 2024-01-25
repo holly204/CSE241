0d33b93f9cdde9da862c81d266c60d05876235df
RuihaoWei95 <ruihw95@gmail.com>
2024-01-25 15:10:48 -0800

    Update cpu_testcase_1.sh
    add sd calculation function

--- HW1/shellScripts/cpu_testcase_1.sh
+++ HW1/shellScripts/cpu_testcase_1.sh
@@ -36,9 +36,18 @@ min_eps=$(printf "%s\n" "${events_per_sec[@]}" | sort -n | head -n1)
 max_eps=$(printf "%s\n" "${events_per_sec[@]}" | sort -n | tail -n1)
 avg_eps=$(calculate_average)
 
+# Calculating standard deviation
+sum_sq=0
+for t in "${events_per_sec[@]}"; do
+    deviation=$(echo "$t - $avg_eps" | bc)
+    sum_sq=$(echo "$sum_sq + ($deviation)^2" | bc)
+done
+std_dev=$(echo "scale=2; sqrt($sum_sq / ${#events_per_sec[@]})" | bc)
+
 # Reporting results
 echo "Performance Test Results for Events Per Second:"
 echo "Average: $avg_eps"
 echo "Minimum: $min_eps"
 echo "Maximum: $max_eps"
+echo "Standard Deviation: $std_dev"
 
